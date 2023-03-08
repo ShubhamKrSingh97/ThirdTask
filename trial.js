627f2552b45c23380543ce8b9f411a36bd108ab3
ShubhamKrSingh97 <shubhkrsingh.1997@gmail.com>
2023-03-08 20:24:31 +0530

    Web dev task

---
+++ trial.js
@@ -0,0 +1,4 @@
+var head=document.querySelector('#main-header');
+head.style.borderBottom='solid 5px black';
+document.querySelector('.title').style.fontWeight='bold';
+document.querySelector('.title').style.color='green';
