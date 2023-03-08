5f87907e5d9a82b1a31da15c9e035e6dfbc91acf
ShubhamKrSingh97 <shubhkrsingh.1997@gmail.com>
2023-03-08 22:19:48 +0530

    added hey

--- trial.js
+++ trial.js
@@ -1,11 +1,3 @@
-var head=document.querySelector('#main-header');
-head.style.borderBottom='solid 5px black';
-document.querySelector('.title').style.fontWeight='bold';
-document.querySelector('.title').style.color='green';
-var list=document.querySelector('.list-group');
-list.children[2].style.backgroundColor='green';
-var items=document.querySelectorAll('.list-group-item');
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-    items[i].style.color='red';
-}
+var items=document.querySelector('ul');
+items.innerHTML+='<li>Hey</li>';
+
