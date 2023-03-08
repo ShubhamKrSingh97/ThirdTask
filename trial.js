f9b92163745de8217836e5405b2d23b9e589ad81
ShubhamKrSingh97 <shubhkrsingh.1997@gmail.com>
2023-03-08 21:02:46 +0530

    Made font bold and item 3 has bg green now

--- trial.js
+++ trial.js
@@ -2,3 +2,10 @@ var head=document.querySelector('#main-header');
 head.style.borderBottom='solid 5px black';
 document.querySelector('.title').style.fontWeight='bold';
 document.querySelector('.title').style.color='green';
+var list=document.querySelector('.list-group');
+list.children[2].style.backgroundColor='green';
+var items=document.querySelectorAll('.list-group-item');
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+    items[i].style.color='red';
+}
