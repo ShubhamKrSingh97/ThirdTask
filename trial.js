0ed54329994d02d1a028b8e902b7679955533ff0
ShubhamKrSingh97 <shubhkrsingh.1997@gmail.com>
2023-03-09 20:47:12 +0530

    Manipulated Dom using the keywords

--- trial.js
+++ trial.js
@@ -1,6 +1,21 @@
-var odd=document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='green';
-}
+var par=document.querySelector('#items');
+par.parentElement.style.backgroundColor='red';
+var last=document.querySelector('.card-body');
+last.lastElementChild.style.color='blue';
+last.firstElementChild.style.color='blue';
+document.querySelector('.mb-3').nextElementSibling.style.color='green';
+var add=document.createElement('li');
+add.setAttribute('class','new-element');    
+add.textContent='New item';
+par.appendChild(add);
+
+var hello=document.querySelector('.container');
+var ad=document.createElement('li');
+ad.textContent='Hello World';
+var hi=document.querySelector('header h1');
+hello.insertBefore(ad,hi);
+var hey=document.querySelector('#items');
+var lol=document.querySelector('.list-group-item');
+hey.insertBefore(ad,lol);
 
 
