f8e2f58d7de51068985c867d9df2cab397bf3021
ShubhamKrSingh97 <shubhkrsingh.1997@gmail.com>
2023-03-09 14:25:06 +0530

    Odd li's bg changed to green

--- trial.js
+++ trial.js
@@ -1,4 +1,6 @@
-var items=document.querySelectorAll('.list-group-item');
-items[1].style.backgroundColor='green';
-items[2].style.display='none';
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
+
 
